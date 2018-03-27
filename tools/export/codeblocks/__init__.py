1def39c49f6bb87129a99b059c6598040272f1b4
Thomas Sailer <t.sailer@alumni.ethz.ch>
2018-03-27 12:44:07 +0200

    work on NCS36510 checksum

--- tools/export/codeblocks/__init__.py
+++ tools/export/codeblocks/__init__.py
@@ -37,13 +37,27 @@ class CodeBlocks(GccArm):
         "NCS36510TargetCode.ncs36510_addfib"
     ])
 
+    @classmethod
+    def is_target_supported(cls, target_name):
+        target = TARGET_MAP[target_name]
+        return apply_supported_whitelist(
+            cls.TOOLCHAIN, cls.POST_BINARY_WHITELIST, target)
+
+    @staticmethod
+    def filter_dot(str_in):
+        """
+        Remove the './' prefix, if present.
+        This function assumes that resources.win_to_unix()
+        replaced all windows backslashes with slashes.
+        """
+        if str_in is None:
+            return None
+        if str_in[:2] == './':
+            return str_in[2:]
+        return str_in
+
     def generate(self):
         self.resources.win_to_unix()
-
-        sources         = [] # list of strings
-
-        for r_type in ['headers', 'c_sources', 's_sources', 'cpp_sources']:
-            sources.extend(getattr(self.resources, r_type))
 
         comp_flags = []
         debug_flags = []
@@ -53,6 +67,8 @@ class CodeBlocks(GccArm):
             if f == "-include":
                 next_is_include = True
                 continue
+            if f == 'c':
+                continue
             if next_is_include:
                 f = '-include ' + f
             next_is_include = False
@@ -61,23 +77,33 @@ class CodeBlocks(GccArm):
                 continue
             comp_flags.append(f)
         comp_flags = list(set(comp_flags))
+        inc_dirs = [self.filter_dot(s) for s in self.resources.inc_dirs];
+        inc_dirs = [x for x in inc_dirs if x is not None and x != '' and x != '.' and not x.startswith('bin') and not x.startswith('obj')];
+
+        c_sources = [self.filter_dot(s) for s in self.resources.c_sources]
+        ncs36510fib = hasattr(target, "post_binary_hook") and getattr(target, "post_binary_hook") == 'NCS36510TargetCode.ncs36510_addfib'
+        if ncs36510fib:
+            c_soures.append('ncs36510fib.c')
+            c_soures.append('ncs36510trim.c')
 
         ctx = {
             'project_name': self.project_name,
             'debug_flags': debug_flags,
             'comp_flags': comp_flags,
             'ld_flags': self.flags['ld_flags'],
-            'headers': list(set(self.resources.headers)),
-            'c_sources': self.resources.c_sources,
-            's_sources': self.resources.s_sources,
-            'cpp_sources': self.resources.cpp_sources,
-            'sources': sources,
-            'include_paths': self.resources.inc_dirs,
-            'linker_script': self.resources.linker_script,
+            'headers': list(set([self.filter_dot(s) for s in self.resources.headers])),
+            'c_sources': c_sources,
+            's_sources': [self.filter_dot(s) for s in self.resources.s_sources],
+            'cpp_sources': [self.filter_dot(s) for s in self.resources.cpp_sources],
+            'include_paths': inc_dirs,
+            'linker_script': self.filter_dot(self.resources.linker_script),
             'libraries': self.resources.libraries
             }
 
         self.gen_file('codeblocks/cbp.tmpl', ctx, "%s.%s" % (self.project_name, 'cbp'))
+        if ncs36510fib:
+            for f in [ 'ncs36510fib.c', 'ncs36510trim.c' ]:
+                self.gen_file("codeblocks/%s" % f, ctx, f)
 
         # finally, generate the Makefile
         super(CodeBlocks, self).generate()
@@ -87,5 +113,7 @@ class CodeBlocks(GccArm):
         for ext in ['cbp', 'depend', 'layout']:
             remove("%s.%s" % (project_name, ext))
         remove('openocd.log')
+        remove('ncs36510fib.c')
+        remove('ncs36510trim.c')
         rmtree('bin', ignore_errors=True)
         rmtree('obj', ignore_errors=True)
